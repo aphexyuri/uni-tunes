15653e13d54a7134995621cd7aa67e2f03f0a74a
Yuri Visser <yuri@scopely.com>
2014-07-22 12:49:34 -0400

    fixed editor btn detection

--- Assets/UniTunes/Scripts/Player/UniTunesBtn.cs
+++ Assets/UniTunes/Scripts/Player/UniTunesBtn.cs
@@ -6,29 +6,34 @@ public class UniTunesBtn : MonoBehaviour
 	public delegate void OnBtnPressed(string btnName);
 	public static event OnBtnPressed OnBtnPressedEvt;
 
-#if UNITY_EDITOR
-	void OnMouseUpAsButton()
-	{
-		OnBtnPressedEvt(name);
-	}
-#endif
+	private Camera _cam;
 
-#if !UNITY_EDITOR
-	void Update()
+	void Awake()
 	{
-		if (Input.touchCount == 1)
-		{
-			if(Input.GetTouch(0).phase == TouchPhase.Ended) {
-				Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-				Vector2 touchPos = new Vector2(wp.x, wp.y);
-			
-				Collider2D c2d = Physics2D.OverlapPoint(touchPos);
-
-				if(collider2D == c2d) {
-					OnBtnPressedEvt(name);
-				}
+		foreach(Camera cam in Camera.allCameras) {
+			if(cam.name == "SoundCloudPlayer") {
+				_cam = cam;
 			}
 		}
 	}
+
+	void Update()
+	{
+#if UNITY_EDITOR
+		if(Input.GetMouseButtonUp(0)) {
+#else
+		if (Input.touchCount == 1) {
+			if(Input.GetTouch(0).phase == TouchPhase.Ended) {
 #endif
+				Vector3 touchPos = _cam.ScreenToWorldPoint(Input.mousePosition);
+				RaycastHit2D hit = Physics2D.Raycast(touchPos, transform.position);
+
+				if(hit.collider != null && hit.collider == collider2D) {
+					OnBtnPressedEvt(name);
+				}
+#if !UNITY_EDITOR
+			}
+#endif
+		}
+	}
 }
