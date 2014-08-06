e86e59807affda62f6316f3edbb69cf9757bfb84
Yuri Visser <yuri@scopely.com>
2014-08-06 16:50:32 -0300

    change SCSetJsonModel level for player visiblitiy

---
+++ Assets/UniTunes/Scripts/Models/SCSetJsonModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+public class SCSetJsonModel
+{
+	public SCSetJsonModel() {}
+
+	public SCSetJsonModel(SCSet scSet)
+	{
+		tracks = scSet.tracks;
+		loopPlaylist = scSet.loopPlaylist;
+	}
+
+	public List<SCTrack> tracks;
+	
+	public bool loopPlaylist;
+
+	public SCTrack GetTrackAtIndex(int index)
+	{
+		if(tracks != null) {
+			if(index < tracks.Count) {
+				return tracks[index];
+			}
+		}
+
+		return null;
+	}
+}
